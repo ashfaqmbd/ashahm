c6c1dc06dd7decbab1dae4b796d05fa9335caace
Ashfaq Ahmed <ashfaqmbd@gmail.com>
2022-03-26 23:45:57 -0700

    homepage in package.json

--- src/App.js
+++ src/App.js
@@ -3,21 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Ashfaq Ahmed</h1>
+      <h3>Frontend Web Developer-Pakistan</h3>
     </div>
   );
 }
